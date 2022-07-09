3f132c691b17d45412ed0f88bfa07a0517120e68
Lukas Sandman <sandmanlukas@gmail.com>
2022-07-09 19:57:20 +0200

    api logic done, command line argument logic done. next step is to display information nicely

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,7 @@
-use serde::Deserialize;
+//use serde::Deserialize;
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+use serde_json::Value;
 use reqwest;
 use std::env;
 //use curl::easy::{Easy,List};
@@ -22,10 +25,6 @@ struct Vasttrafik {
     expires_in: u32
 }
 
-//#[derive(Deserialize)]
-//struct DepartureBoard {
-//    departure: Vec<T>
-//}
 
 pub async fn get_access_token() -> Result<String,reqwest::Error>{
     let secret = get_env_token();
@@ -41,41 +40,156 @@ pub async fn get_access_token() -> Result<String,reqwest::Error>{
     let access_token = res.access_token;
     let expires_in = res.expires_in; //TODO: add code to check if token is about to expire, <60s left?
 
-    println!("token: {}, expires: {}", access_token, expires_in);
+    //println!("token: {}, expires: {}", access_token, expires_in);
     Ok(access_token)
 
 }
 
 
-//https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=test&date=2022-07-08&time=22%3A02&format=json
 
-pub async fn get_departure_board(token: String, id: String, date: String, time: String) -> Result<String, reqwest::Error>
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DepartureRoot {
+    #[serde(rename = "DepartureBoard")]
+    pub departure_board: DepartureBoard,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct DepartureBoard {
+    #[serde(rename = "Departure")]
+    pub departure: Vec<Departure>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Departure {
+    pub name: String,
+    pub sname: String,
+    pub journey_number: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    //pub stopid: String,
+    pub stop: String,
+    pub time: String,
+    pub date: String,
+    //pub journeyid: String,
+    pub direction: String,
+    pub track: String,
+    //pub rt_time: Option<String>,
+    //pub rt_date: Option<String>,
+    //pub fg_color: String,
+    //pub bg_color: String,
+    //pub stroke: String,
+    pub accessibility: Option<String>,
+    //#[serde(rename = "JourneyDetailRef")]
+    //pub journey_detail_ref: JourneyDetailRef,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LocationRoot {
+    #[serde(rename = "LocationList")]
+    pub location_list: LocationList,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LocationList {
+    pub no_namespace_schema_location: String,
+    pub servertime: String,
+    pub serverdate: String,
+    #[serde(rename = "StopLocation")]
+    pub stop_location: Option<Vec<StopLocation>>,
+    #[serde(rename = "CoordLocation")]
+    pub coord_location: Option<Vec<CoordLocation>>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct StopLocation {
+    pub name: String,
+    pub lon: String,
+    pub lat: String,
+    pub id: String,
+    pub idx: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CoordLocation {
+    pub name: String,
+    pub lon: String,
+    pub lat: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    pub idx: String,
+}
+
+
+pub async fn get_departure_board(token: &String, id: String, date: String, time: String) -> Result<Vec<Departure>, reqwest::Error>
 {
     let client = reqwest::Client::new();
 
     let base_url = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?";
     let id_url = format!("id={}&",id);
     let date_url = format!("date={}&",date);
+
     let time_url = format!("time={}&",time);
     let format_url = "format=json";
     let url = format!("{}{}{}{}{}",base_url,id_url,date_url,time_url,format_url);
 
-    println!("url: {}", url);
     let res = client.get(&url)
         .header("Authorization", "Bearer ".to_owned() + &token)
         .send()
-        .await?
-        .text() // TODO: change this to json.
         .await?;
+    let status = res.status();
 
+    if !status.is_success(){
+        panic!("Something went wrong! Status: {:?}", status);
+    }
 
-    println!("{:?}",res);
-    Ok("test".to_string())
-
+    let res_json = res.json::<DepartureRoot>().await?;
+    let departures = res_json.departure_board.departure;
+    Ok(departures)
 }
 
+//https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=Chalmers&format=json
 
 
+// The API call pattern matches on the inputted stop name and tries to find the best possible match
+// This function returns the id of most probable match, returns an error if no possible matches are returned.
+pub async fn get_location_id(token: &String, stop: String) -> Result<String, reqwest::Error>{
+    let client = reqwest::Client::new();
 
+    let base_url = "https://api.vasttrafik.se/bin/rest.exe/v2/location.name?";
+    let stop_url = format!("input={}&", stop);
+    let format_url = "format=json";
+    let url = format!("{}{}{}", base_url, stop_url, format_url);
+    
+    let res = client.get(&url)
+        .header("Authorization", "Bearer ".to_owned() + &token)
+        .send()
+        .await?;
 
+    let status = res.status();
 
+    if !status.is_success(){
+        panic!("Something went wrong! Status: {:?}", status);
+    }
+
+    let res_json = res.json::<LocationRoot>().await?;
+
+    let stop_locations = &res_json.location_list.stop_location;
+
+    let location_id = match stop_locations {
+        Some(p) => p[0].id.to_string(),
+        None => panic!("The stop {} doesn't exist. Try another one", stop),
+    };
+
+    //let location_id = &stop_locations[0].id.to_string();
+
+    Ok(location_id)
+
+}
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,68 @@
 mod api;
 
 use clap::Parser;
-//use anyhow::{Context, Result};
+use chrono::prelude::*;
 
-/// Show next departing trams/buses/etc from given station in Gothenburg.
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Cli {
-    /// The tram station to display info about
-    station: String,
+
+fn validate_station(station: &str) -> Result<(), String> {
+    if station.trim().len() != station.len() {
+        Err(String::from(
+            "Station cannot have leading and trailing space",
+        ))
+    } else {
+        Ok(())
+    }
+}
+
+fn validate_date(date: &str) -> Result<(), String>{
+    match NaiveDate::parse_from_str(date, "%Y-%m-%d"){
+        Ok(v) => Ok(()),
+        Err(e) => Err(String::from("Incorrect date format, date must be in YYYY-MM-DD format. Please try again.")),
+    }
 
 }
+
+fn validate_time(time: &str) -> Result<(), String>{
+    match NaiveTime::parse_from_str(time, "%H:%M"){
+        Ok(v) => Ok(()),
+        Err(e) => Err(String::from("Incorrect time format, time must be in HH:MM format. Please try again.")),
+    }
+
+}
+
+/// Show next departing trams/buses/etc from given station in Gothenburg.
+#[derive(Parser, Debug, Default)]
+#[clap(author="Lukas Sandman", version="1.0", about="A simple CLI-app to get information about coming trams and buses in Gothenburg.", long_about = None)]
+struct Cli {
+    #[clap(forbid_empty_values = true, validator=validate_station)]
+    /// The tram station to display info about
+    station: String,
+    #[clap(short, long,validator=validate_time)]
+    /// If specified, displays information on the coming trams/buses 20min from specified time. Time must be in HH:MM format. If not specified, uses the current time.
+    time: Option<String>,
+    #[clap(short,long, validator=validate_date)]
+    /// If specified, displays information on the coming trams/buses on specified date. Date must be in YYYY-MM-DD format. If not specified, uses current date.
+    date: Option<String>,
+
+
+}
+
 
 #[tokio::main]
 async fn main() {
     let args = Cli::parse();
     let station = args.station;
-    println!("Station: {}", &station);
+    let dt = Utc::now();
 
+    let time = match args.time {
+        Some(p) => p,
+        None => dt.format("%H:%M").to_string(),
+    };
+
+    let date = match args.date {
+        Some(p) => p,
+        None => dt.format("%Y-%m-%d").to_string(),
+    };
 
     let result = api::get_access_token();
     let token = match result.await {
@@ -27,9 +72,25 @@ async fn main() {
         Err(e) => panic!("error: {}", e),
     };
 
-    let departure = match api::get_departure_board(token,"Chalmers".to_string(), "2022-07-09".to_string(), "12:12".to_string()).await{
+    let location_id = match api::get_location_id(&token, station
+    ).await {
+        Ok(id) => id,
+        Err(e) => panic!("error: {}", e),
+    };
+
+    let departures = match api::get_departure_board(&token,location_id,date , time).await{
         Ok(res) => res,
         Err(e) => panic!("error: {}", e),
     };
 
+    println!("departures: {:#?}", departures);
+
 }
+
+
+/*
+Line Destination Next(min) Location
+-----------------------------------
+7    Nordstan      3 ♿       A
+8    Brunnsparken  4         C
+*/
