a25e3a8de7e32c8db93e886f1bc659fd24ae96d2
Lukas Sandman <sandmanlukas@gmail.com>
2022-07-14 20:36:54 +0200

    update install.sh

--- ci/install.sh
+++ ci/install.sh
@@ -31,7 +31,7 @@ main() {
     esac
 
     # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/sandmanlukas/tram-checker \
                        | cut -d/ -f3 \
                        | grep -E '^v[0.1.0-9.]+$' \
                        | $sort --version-sort \
@@ -39,7 +39,7 @@ main() {
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
-           --git japaric/cross \
+           --git sandmanlukas/tram-checker \
            --tag $tag \
            --target $target
 }
